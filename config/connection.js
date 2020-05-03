b46a5991919b7c80b880671af7cad9acc5932a91
rashu <rashuzak2@gmail.com>
2020-05-02 19:57:19 -0500

    connecton to Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // Require mysql
 var mysql = require("mysql");
 
-// Set up our connection information
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "sakammar",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Set up our connection information
+    connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "sakammar",
+      database: "burgers_db"
+    });
+}
+
 
 // Connect to the database
 connection.connect(function(err) {
